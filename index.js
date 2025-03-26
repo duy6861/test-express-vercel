061acb797a92b7e6433dc1749a7d631745912fea
duy6861 <duy6861@gmail.com>
2025-03-26 23:32:58 +0700

    push

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const product = require("./phone")
+
+const app = express();
+app.use(express.json());
+app.use("/product", product)
+
+const port = process.env.PORT || 9001
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ phone.js
@@ -0,0 +1,60 @@
+const product = [
+  {
+    id: 1,
+    name: "Iphone 16 Pro",
+    desc: "6.1 inch display",
+    price: 799,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741631849/iphone-16-pro_exmxux.png"
+  },
+  {
+    id: 2,
+    name: "Samsung S22 Ultra",
+    desc: "6.8 inch display",
+    price: 999,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741886438/vn-galaxy-s25-s938-sm-s938bztcxxv-thumb-544711852_gvxvng.png"
+  },
+  {
+    id: 3,
+    name: "OnePlus 9 Pro",
+    desc: "6.7 inch display",
+    price: 699,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741631849/OnePlus_9-pro_dmzgho.png"
+  },
+  {
+    id: 4,
+    name: "Google Pixel 6 Pro",
+    desc: "6.7 inch display",
+    price: 899,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741631850/pixel-6-header_n826xz.png"
+  },
+  {
+    id: 5,
+    name: "Xiaomi Redmi Note 11 Pro",
+    desc: "6.7 inch display",
+    price: 499,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741888346/note-11-pro_hkesp8.png"
+  },
+  {
+    id: 6,
+    name: "Samsung S21 Ultra",
+    desc: "6.8 inch display",
+    price: 999,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741888307/galaxy-s23-ultra_rvt1b1.png"
+  },
+  {
+    id: 7,
+    name: "OnePlus 8 Pro",
+    desc: "6.7 inch display",
+    price: 699,
+    cartQuantity: 0,
+    img: "https://res.cloudinary.com/dl9kkwfyw/image/upload/v1741631849/OnePlus_9-pro_dmzgho.png"
+  }
+
+]
+module.exports = product
