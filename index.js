0d84e4c3ee4841cb118dcc1b260fd36eda8464b6
duy6861 <duy6861@gmail.com>
2025-03-27 00:12:26 +0700

    push2

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors"); // Import middleware CORS
 const productRoutes = require("./phone"); // Import Router
 
 const app = express();
+
+// Sử dụng CORS để cho phép tất cả các origin truy cập
+app.use(cors());
+
+// Middleware cho JSON
 app.use(express.json());
-app.use("/product", productRoutes); // Đăng ký Router
+
+// Đăng ký Router
+app.use("/product", productRoutes);
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => {
